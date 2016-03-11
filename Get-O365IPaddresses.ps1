f448a60022faf1a89c276ac897a8965c8495420a
Gavin Morrison <GavinMorrison@users.noreply.github.com>
2016-03-11 20:18:27 +0000

    get-o365ipaddresses.ps1 initial version

---
+++ Get-O365IPaddresses.ps1
@@ -0,0 +1,28 @@
+function Get-O365IPAddresses {
+[CmdletBinding()]
+param
+(
+    [ValidateSet('o365','LYO','ProPlus','SPO','WAC','EX-Fed','OfficeiPad','OfficeMobile','Yammer','RCA','EOP')]
+    [String[]]
+    $Product,
+    [ValidateSet('IPV4','IPV6','URL')]
+    [String[]]
+    $AddressType
+)
+	$O365IPAddresses = [xml](Invoke-WebRequest -UseBasicParsing -Uri 'https://support.content.office.net/en-us/static/O365IPAddresses.xml')
+    $Updated = ([datetime]$O365IPAddresses.products.updated)
+    $O365IPAddresses.products.product | % {if($Product.Count -ge 1 -and $Product -eq $_.name) {$_} elseif(!($Product.Count -ge 1)) {$_}} | % {
+        $xmlProduct = $_.name
+        $_.addresslist | % {if($AddressType.Count -ge 1 -and $AddressType -eq $_.type) {$_} elseif(!($AddressType.Count -ge 1)) {$_}} | % {
+            $xmlAdressType = $_.type
+            $_.address | % {
+                [PSCustomObject]@{
+                    'Product' = $xmlProduct;
+                    'AddressType' = $xmlAdressType;
+                    'Address' = $_;
+                    'Updated' = $Updated;
+                }
+            }
+        }
+    }
+}
