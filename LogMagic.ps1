0b0f511a4db5c978348aa30e3427046f3425ec2d
Gavin Morrison <gavinmorrison@users.noreply.github.com>
2016-06-24 08:08:05 +0100

    updated LogMagic

--- LogMagic.ps1
+++ LogMagic.ps1
@@ -2,25 +2,77 @@ function Write-Everything {
 [CmdletBinding()]
 param()
     Write-Output 'This is an output.'
-    Start-Sleep -Seconds 2
     Write-Warning 'This is a warning.'
-    Start-Sleep -Seconds 4
     Write-Verbose 'This is a verbose.'
+    Write-Information 'info log!'
+    Write-Error 'This is an error.'
 }
 
-function PipelineExplorer {
+function Out-LogFile {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(ValueFromPipeline)]$PipelineInput,
+        $OutputLogPath,
+        $ErrorLogPath,
+        $WarningLogPath,
+        $VerboseLogPath,
+        $DebugLogPath,
+        $InfoLogPath,
+        $GlobalLogPath,
+        [switch]$SuppressOutput
+    )
+    Begin
+    {
+        function New-LogOutputString {
+            param($LogMessage, $OutputLevel)
+            return "[$(Get-Date)][$($OutputLevel)] $($LogMessage)"
+        }
+    }
     Process
     {
-        $InputFromPipeline = $_
-        switch($_.GetType().Name)
+        switch($PipelineInput.GetType().Name)
         {
-            'String' {$OutputMessage = $InputFromPipeline; $LogLevel = 'INFO'; Write-Output $OutputMessage}
-            'WarningRecord' {$OutputMessage = $InputFromPipeline.Message; $LogLevel = 'WARN'; Write-Warning $OutputMessage}
-            'VerboseRecord' {$OutputMessage = $InputFromPipeline.Message; $LogLevel = 'VERBOSE'; Write-Verbose $OutputMessage}
+            'String'
+            {
+                $LogMessage = $PipelineInput
+                $OutputLevel = 'LOG'
+                if(!$SuppressOutput) { Write-Output -InputObject $LogMessage }
+            }
+            'ErrorRecord'
+            {
+                $LogMessage = $PipelineInput.Exception
+                $OutputLevel = 'ERROR'
+                if(!$SuppressOutput) { Write-Error -Message $LogMessage -ErrorAction Continue }
+            }
+            'WarningRecord'
+            {
+                $LogMessage = $PipelineInput.Message
+                $OutputLevel = 'WARN'
+                if(!$SuppressOutput) { Write-Warning -Message $LogMessage }
+            }
+            'VerboseRecord'
+            {
+                $LogMessage = $PipelineInput.Message
+                $OutputLevel = 'VERBOSE'
+                if(!$SuppressOutput) { Write-Verbose -Message $LogMessage }
+            }
+            'DebugRecord'
+            {
+                $LogMessage = $PipelineInput.Message
+                $OutputLevel = 'DEBUG'
+                if(!$SuppressOutput) { Write-Debug -Message $LogMessage }
+            }
+            'InformationRecord'
+            {
+                $LogMessage = $PipelineInput
+                $OutputLevel = 'INFO'
+                if(!$SuppressOutput) { Write-Information -MessageData $LogMessage }
+            }
         }
-
-        Write-Output "[$(Get-Date)][$($LogLevel)] $($OutputMessage)"
+        Write-Host (New-LogOutputString -LogMessage $LogMessage -OutputLevel $OutputLevel)
     }
 }
 
-Write-Everything -verbose *>&1 | PipelineExplorer
+
+Write-Everything -verbose *>&1 | Out-LogFile
